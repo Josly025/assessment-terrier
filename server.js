2b370fbba3081078efcfb0b4137dd0ce13e77acf
Grantjos12 <grantjoslyn@gmail.com>
2021-09-08 15:31:37 -0500

    inital express server with nodemon

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
